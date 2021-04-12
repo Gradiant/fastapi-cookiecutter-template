e1e7d9692c978bd53cc08c31034ca8cc82adb500
David-Lor <17401854+David-Lor@users.noreply.github.com>
2021-04-12 20:14:23 +0200

    Correct logger module

--- {{cookiecutter.directory_name}}/{{cookiecutter.project_slug}}/logger.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.project_slug}}/logger.py
@@ -4,18 +4,16 @@ from loguru import logger
 
 from .settings import request_logging_settings as settings
 
-LoggerFormat = "<green>{time:YY-MM-DD HH:mm:ss}</green> | " \
-               "<level>{level}</level> | " \
-               "<level>{message}</level> | " \
-               "{extra} {exception}"
+LoggerFormat = "<green>{time:YY-MM-DD HH:mm:ss.SSS}</green> | " \
+               "<level>{level: <8}</level> | " \
+               "<level>{message}</level> | {extra}"
 
 logger.remove()
 logger.add(
-    # TODO Review usage of sys.stderr
-    # TODO review format of LoggerFormat
     sys.stderr,
     level=settings.level.upper(),
     format=LoggerFormat,
     serialize=settings.serialize,
+    enqueue=True,  # process logs in background
     diagnose=False  # hide variable values in log backtrace
 )
